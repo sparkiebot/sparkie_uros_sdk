4fce1b800e1dbdb6113a4938882bbaac31b1a2af
Mattia Sicoli <mattia.sicoli@gmail.com>
2023-10-19 11:59:20 +0200

    ADD: library configuration

---
+++ libmicroros/include/irobot_create_msgs/msg/detail/hazard_detection_vector__rosidl_typesupport_microxrcedds_c.h
@@ -0,0 +1,39 @@
+// generated from rosidl_typesupport_microxrcedds_c/resource/idl__rosidl_typesupport_c.h.em
+// with input from irobot_create_msgs:msg/HazardDetectionVector.idl
+// generated code does not contain a copyright notice
+#ifndef IROBOT_CREATE_MSGS__MSG__HAZARD_DETECTION_VECTOR__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
+#define IROBOT_CREATE_MSGS__MSG__HAZARD_DETECTION_VECTOR__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
+
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "irobot_create_msgs/msg/rosidl_typesupport_microxrcedds_c__visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_irobot_create_msgs
+size_t get_serialized_size_irobot_create_msgs__msg__HazardDetectionVector(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_irobot_create_msgs
+size_t max_serialized_size_irobot_create_msgs__msg__HazardDetectionVector(
+  bool * full_bounded,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_irobot_create_msgs
+const rosidl_message_type_support_t *
+  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_microxrcedds_c, irobot_create_msgs, msg, HazardDetectionVector)();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  // IROBOT_CREATE_MSGS__MSG__HAZARD_DETECTION_VECTOR__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
